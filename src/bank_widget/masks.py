1fde37e476397f36a4986cd3f98d54fdadb3a8c8
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-04-21 01:51:04 +0500

    отредактирован и протестирован модуль masks.py

--- src/bank_widget/masks.py
+++ src/bank_widget/masks.py
@@ -3,11 +3,25 @@
 
 def get_mask_card_number(card_number: str) -> str:
     """Возвращает замаскированный номер карты"""
-    mask_card_number = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    if len(card_number) < 12 or len(card_number) > 19:
+        return card_number
+
+    if not card_number.isdigit():
+        return card_number
+
+    if len(card_number) > 12:
+        mask_stars = "*" * (len(card_number) - 12) + " "
+    else:
+        mask_stars = ""
+
+    mask_card_number = card_number[:4] + " " + card_number[4:6] + "** " + mask_stars + card_number[-4:]
     return mask_card_number
 
 
 def get_mask_account(user_account: str) -> str:
     """Возвращает замаскированный номер счета"""
+    if (len(user_account) != 20) or (not user_account.isdigit()):
+        return user_account
+
     mask_account = "**" + user_account[-4:]
     return mask_account
---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def standard_card_number():
+    return "7000792289606361"
+
+
+@pytest.fixture
+def standard_user_account():
+    return "73654108430135874305"
