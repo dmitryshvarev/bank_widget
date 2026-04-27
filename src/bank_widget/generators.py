6710d81b45faae858631bea4888e724a807aff8c
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-04-28 04:24:06 +0500

    пакеты src и tests отформатированы и проверены линтерами

--- src/bank_widget/generators.py
+++ src/bank_widget/generators.py
@@ -1,11 +1,12 @@
 """Содержит функции для работы с массивами транзакций"""
 
-from typing import Any, Dict, List, Iterator
+from typing import Any, Dict, Iterator, List
+
 
 def filter_by_currency(transactions: List[Dict[str, Any]], currency: str = "USD") -> Iterator:
     """Возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной"""
     for transaction in transactions:
-        if transaction.get("operationAmount").get("currency").get("code") == currency:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
             yield transaction
 
 
@@ -17,6 +18,8 @@ def transaction_descriptions(transactions: List[Dict[str, Any]]) -> Iterator:
 
 def card_number_generator(begin: int, end: int) -> Iterator:
     """Генерирует номера карт в заданном диапазоне"""
-    return (f"{num:016}"[:4] + ' ' + f"{num:016}"[4:8] + ' ' + f"{num:016}"[8:12] + ' ' + f"{num:016}"[12:]
-            for num in range(begin, end+1)
-            if begin > 0 and end < 10**16 and begin <= end)
+    return (
+        f"{num:016}"[:4] + " " + f"{num:016}"[4:8] + " " + f"{num:016}"[8:12] + " " + f"{num:016}"[12:]
+        for num in range(begin, end + 1)
+        if begin > 0 and end < 10**16 and begin <= end
+    )
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,94 +1,71 @@
 import pytest
 
-from src.bank_widget.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.bank_widget.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency_usd(transactions_):
 
     usd_transactions = filter_by_currency(transactions_, "USD")
     assert next(usd_transactions) == {
-          "id": 939719570,
-          "state": "EXECUTED",
-          "date": "2018-06-30T02:08:58.425572",
-          "operationAmount": {
-              "amount": "9824.07",
-              "currency": {
-                  "name": "USD",
-                  "code": "USD"
-              }
-          },
-          "description": "Перевод организации",
-          "from": "Счет 75106830613657916952",
-          "to": "Счет 11776614605963066702"
-      }
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
     assert next(usd_transactions) == {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       }
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
 
 
 def test_filter_by_currency_rub(transactions_):
 
     rub_transactions = filter_by_currency(transactions_, "RUB")
     assert next(rub_transactions) == {
-                "id": 873106923,
-                "state": "EXECUTED",
-                "date": "2019-03-23T01:09:46.296404",
-                "operationAmount": {
-                    "amount": "43318.34",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод со счета на счет",
-                "from": "Счет 44812258784861134719",
-                "to": "Счет 74489636417521191160"
-            }
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    }
     assert next(rub_transactions) == {
-                "id": 594226727,
-                "state": "CANCELED",
-                "date": "2018-09-12T21:27:25.241689",
-                "operationAmount": {
-                    "amount": "67314.70",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод организации",
-                "from": "Visa Platinum 1246377376343588",
-                "to": "Счет 14211924144426031657"
-            }
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    }
 
 
 def test_transaction_descriptions(transactions_):
-    assert list(transaction_descriptions(transactions_)) == ["Перевод организации",
-                                                             "Перевод со счета на счет",
-                                                             "Перевод со счета на счет",
-                                                             "Перевод с карты на карту",
-                                                             "Перевод организации",
-                                                             ]
+    assert list(transaction_descriptions(transactions_)) == [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
+    ]
 
 
 @pytest.mark.parametrize(
     "begin_, end_, card_num",
     [
         (1, 3, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003"]),
-        (10**16-3, 10**16-1, ["9999 9999 9999 9997", "9999 9999 9999 9998", "9999 9999 9999 9999"]),
+        (10**16 - 3, 10**16 - 1, ["9999 9999 9999 9997", "9999 9999 9999 9998", "9999 9999 9999 9999"]),
         (0, 3, []),
-        (10**16-3, 10**16, []),
+        (10**16 - 3, 10**16, []),
         (2, 1, []),
     ],
 )
